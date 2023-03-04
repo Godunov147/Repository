b80c95c6b3a35a5edbe6a2be5875c1cc1e424a0b
Maksim <godunov_05@inbox.ru>
2023-03-05 00:46:02 +0300

    fun

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int len = scanner.nextInt();
+
+        if (len == 0 | len == 1){
+            return;
+        }
+        int summa = 0;
+        for(int i = 0; i < len; i++){
+            int num = scanner.nextInt();
+            summa = summa + num;
+        }
+        System.out.println(summa);
+
     }
 }
